c51c55f23457d8ddb36588972fcc5cb4f51bd571
wsasouza <wsasouza@hotmail.com>
2022-01-17 19:15:11 -0300

    Feat: Application updated

---
+++ src/config/Constants.ts
@@ -0,0 +1,4 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION: 'mongodb+srv://<user>:<password>@<database>.8b9mj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
